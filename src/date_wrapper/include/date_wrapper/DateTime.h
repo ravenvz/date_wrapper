acc5a83a739c4b12144b4294b97ad1265bdb2132
Pavel <texort@gmail.com>
2017-08-08 15:37:02 +0300

    Fix copyright information

--- src/date_wrapper/include/date_wrapper/DateTime.h
+++ src/date_wrapper/include/date_wrapper/DateTime.h
@@ -1,22 +1,22 @@
 /********************************************************************************
 **
-** Copyright (C) 2016 Pavel Pavlov.
+** Copyright (C) 2016, 2017 Pavel Pavlov.
 **
 **
-** This file is part of SprintTimer.
+** This file is part of DateWrapper.
 **
-** SprintTimer is free software: you can redistribute it and/or modify
+** DateWrapper is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU Lesser General Public License as published by
 ** the Free Software Foundation, either version 3 of the License, or
 ** (at your option) any later version.
 **
-** SprintTimer is distributed in the hope that it will be useful,
+** DateWrapper is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU Lesser General Public License for more details.
 **
 ** You should have received a copy of the GNU Lesser General Public License
-** along with SprintTimer.  If not, see <http://www.gnu.org/licenses/>.
+** along with DateWrapper.  If not, see <http://www.gnu.org/licenses/>.
 **
 *********************************************************************************/
 #ifndef DATETIME_H_RTJVB37W
